75dac8a9db83f959c5e46d4d78461cbed8dee7d8
Willow1000 <wilkinsondari7@gmail.com>
2025-01-07 17:32:24 +0300

    second commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,31 @@
+def display_menu():
+    print('1. Add item')
+    print('2. Remove item')
+    print('3. View list')
+    print('4. Exit')
+
+def main():
+    shopping_list=[]
+    while True:
+        display_menu()
+        choice = int(input('Enter your choice: '))
+
+        if choice == 1:
+            item = input('Enter item to add: ')
+            shopping_list.append(item)
+        elif choice == 2:
+            item = input('Enter item to remove: ')
+            if item in shopping_list:
+                shopping_list.remove(item) 
+            else:
+                print('Item not found')
+        elif choice == 3:
+            print('Shopping list:')
+            for item in shopping_list:
+                print(item)
+
+        else:
+            print("Invalid choice. Kindly read the options and try again.")
+
+if __name__ == '__main__':
+    main()                                
