298fdd3d22a56f3b09ceb789140f4614730d6cea
Willow1000 <wilkinsondari7@gmail.com>
2025-01-15 18:33:37 +0300

    made some changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, balance =0,account_balance:float):
+    def __init__(self,account_balance:float=0):
         self.account_balance = account_balance
 
     def deposit(self, amount):
